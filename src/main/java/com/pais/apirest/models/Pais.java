f5eac368d0ed7b58b2625095555503db160cd6bf
elvis <elvis.fernandes89@gmail.com>
2021-01-15 01:14:47 -0200

    retirada de comentario na classe pais

--- src/main/java/com/pais/apirest/models/Pais.java
+++ src/main/java/com/pais/apirest/models/Pais.java
@@ -12,9 +12,6 @@ import javax.persistence.Table;
 @Table(name = "TB_PAIS")
 public class Pais implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Id
